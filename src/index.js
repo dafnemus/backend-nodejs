c04ae2a6718e366a74ea0109e4402486e2e0bef1
dafnemus <dafnemus@gmail.com>
2023-01-02 12:43:09 -0300

    creacion de modulos locales

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const http = require('http')
+const http = require('http');
+// const fs = require('fs'); // leer atchivos
+// const path = require('path'); // trabaja conjuntamente con fs
 
-const server = http.createServer(function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/html"})
-  res.write("<html><body>HOLA</body></html>")
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<html><body>HOLA</body></html>');
   res.end();
-})
+});
 
-server.listen(5000)
+server.listen(5000);
