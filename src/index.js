db409f79c1a3cbb8a0c256ebd11d63d25448af4d
dafnemus <dafnemus@gmail.com>
2023-01-11 16:33:02 -0300

    added routes and installed phone lib

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ const http = require('http');
 // const fs = require('fs'); // leer atchivos
 // const path = require('path'); // trabaja conjuntamente con fs
 
+const PORT = 5000
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<html><body>HOLA</body></html>');
+
+  if(req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body>HOLA</body></html>');
+  } else if(req.url === '/info') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body>Info</body></html>');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('<html><body>404. Not found</body></html>');
+  }
   res.end();
 });
 
-server.listen(5000);
+server.listen(PORT);
