906ecdfb3b9379be66149105acc792d8248c46ce
dafnemus <dafnemus@gmail.com>
2023-01-11 16:58:07 -0300

    - use switch to routes - Created phone route that it receive value and country queries to get a phone number

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 const http = require('http');
+const url = require('url');
+const phone = require('phone');
 // const fs = require('fs'); // leer atchivos
 // const path = require('path'); // trabaja conjuntamente con fs
 
-const PORT = 5000
+const PORT = 5000;
 const server = http.createServer((req, res) => {
+  const urlData = url.parse(req.url, true);
+  const path = urlData.pathname;
+  const query = urlData.query;
 
-  if(req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body>HOLA</body></html>');
-  } else if(req.url === '/info') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body>Info</body></html>');
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.write('<html><body>404. Not found</body></html>');
+  switch (path) {
+    case '/':
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write('<html><body>HOLA</body></html>');
+    case '/info':
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write('<html><body>Info</body></html>');
+    case '/phone':
+      try {
+        const phoneNumber = phone(query.value, query.country);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(phoneNumber));
+      } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/html' });
+        res.write(`<html><body>Error: ${error.message}</body></html>`);
+      }
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.write('<html><body>404. Not found</body></html>');
   }
   res.end();
 });
