2e7a4a3bc2efb1b8a21770c6a88f5db1b0857923
dafnemus <dafnemus@gmail.com>
2023-01-11 15:26:00 -0300

    - added operations modules in utils folder - change functions to arrow functions

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 // const fs = require('fs'); // leer atchivos
 // const path = require('path'); // trabaja conjuntamente con fs
 
-const server = http.createServer(function (req, res) {
+const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<html><body>HOLA</body></html>');
   res.end();
---
+++ src/utils/operations.js
@@ -0,0 +1,10 @@
+// creacion y esportación de módulos locales
+
+const addition = (num1, num2) => num1 + num2;
+
+const multiplication = (num1, num2) => num1 + num2;
+
+module.exports = {
+  addition,
+  multiplication,
+};
